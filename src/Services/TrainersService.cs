6672d3832c6633a6baa5991f7b2a99e374d23e94
Mike Kistler <mikekistler@microsoft.com>
2024-11-03 11:13:41 -0800

    Drop XML example

--- src/Services/TrainersService.cs
+++ src/Services/TrainersService.cs
@@ -21,10 +21,10 @@ public class TrainersService(TrainingDb trainingDb)
         return TypedResults.Created($"/trainers/{trainer.Id}", trainer);
     }
 
-    public async Task<Results<XmlResult<List<Trainer>>, NotFound>> GetTrainers()
+    public async Task<Ok<List<Trainer>>> GetTrainers()
     {
         var trainers = await trainingDb.Trainers.ToListAsync();
-        return Results.Extensions.Xml(trainers);
+        return TypedResults.Ok(trainers);
     }
 
     public async Task<Results<NoContent, NotFound>> DeleteTrainerById(int id)
